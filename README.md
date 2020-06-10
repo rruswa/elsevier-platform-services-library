a603f8b168e84696b0cc38e340a6b2e585bfa760
Rodney Ruswa <rruswa@outlook.com>
2020-06-10 13:03:57 +0100

    Add public static field token to AuthAPI class which holds a bearer token String and also create README file

---
+++ README.md
@@ -0,0 +1,15 @@
+# Elsevier Technical Test API Library
+
+The purpose of this library is to provide code for calling the API service so that it may be used for API tests or as part of UI automation framework as a means of setting up data, or retrieving resources.
+
+## Requirements
+
+### Tools
+Work in progress...
+### Configuration
+**NOTE:** The values to set for the configuration below will be emailed to Elsevier. They have not been provided here for security reasons.
+
+Please set the following properties as either environment variables or pass them in as a jvm argument upon execution:
+
+**elsevier.bitly.username**: This must be set to the value of the bitly username.
+**elsevier.bitly.password**: This must be set to the password for the bitly username provided.
--- src/main/java/elsevier/uk/entellect/platform/services/AuthAPI.java
+++ src/main/java/elsevier/uk/entellect/platform/services/AuthAPI.java
@@ -1,9 +1,22 @@
 package elsevier.uk.entellect.platform.services;
 
+import elsevier.uk.entellect.platform.utils.PropUtils;
 import io.restassured.RestAssured;
 import org.apache.http.HttpStatus;
 
 public class AuthAPI extends BaseAPI {
+
+    public static String token;
+
+    static {
+        // TODO: Evaluate if this should not be statically done as each JVM instance using this library will result
+        // in this token being retrieved, depending on test environment set up this could be implemented differently to
+        // only be called once for multiple JVM being run concurrently
+        String username = PropUtils.get("elsevier.bitly.username").orElseThrow(IllegalStateException::new);
+        String password = PropUtils.get("elsevier.bitly.password").orElseThrow(IllegalStateException::new);
+
+        token = AuthAPI.bearerToken(username, password);
+    }
 
     public static String bearerToken(String username, String password) {
         // Logging of the response was removed as it exposed the bearer token being returned by this API call.
