d8189ff37a5dc9aae264a1dc8035030038114b9c
Rodney Ruswa <rruswa@outlook.com>
2020-06-12 10:20:27 +0100

    Fix bug in BitlinkAPI#create caused by trailing / at the end of the path

--- src/main/java/elsevier/uk/entellect/platform/services/bitlink/BitlinkAPI.java
+++ src/main/java/elsevier/uk/entellect/platform/services/bitlink/BitlinkAPI.java
@@ -3,35 +3,32 @@ package elsevier.uk.entellect.platform.services.bitlink;
 import elsevier.uk.entellect.platform.models.BitlinkModel;
 import elsevier.uk.entellect.platform.models.LinkModel;
 import elsevier.uk.entellect.platform.services.common.BaseAPI;
+import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.config.EncoderConfig;
 import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
 
-import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.is;
 
 public class BitlinkAPI extends BaseAPI {
 
-    private static RequestSpecBuilder reqSpec = new RequestSpecBuilder().setBasePath("v4/bitlinks/");
+    private static RequestSpecBuilder reqSpec = new RequestSpecBuilder().setBasePath("v4/bitlinks");
 
     public static LinkModel create(BitlinkModel bitlink) {
-        String redirectUrl =  given()
+         return given()
                     .spec(getBaseRequestSpecBuilder().build().spec(reqSpec.build()))
-                .contentType(ContentType.JSON)
+                    .contentType(ContentType.JSON)
+                 .config(RestAssured.config().encoderConfig(EncoderConfig.encoderConfig().appendDefaultContentCharsetToContentTypeIfUndefined(false)))
                 .body(bitlink)
                 .when()
                     .post()
                 .then()
-                    .statusCode(HttpStatus.SC_TEMPORARY_REDIRECT)
-                .extract()
-                .header("Location");
-
-        return with()
-                    .spec(getBaseRequestSpecBuilder().build().spec(reqSpec.build()))
-                    .get(redirectUrl)
-                .then()
-                    .extract()
-                    .body()
-                    .as(LinkModel.class);
+                 .log().all()
+                 .statusCode(anyOf(is(HttpStatus.SC_CREATED), is(HttpStatus.SC_OK)))
+                 .extract().body().as(LinkModel.class);
     }
 
 }
