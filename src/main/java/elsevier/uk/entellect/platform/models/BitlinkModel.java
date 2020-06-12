21b9a3176b43429cab89b56d16d03db391e51d84
Rodney Ruswa <rruswa@outlook.com>
2020-06-12 12:23:14 +0100

    Implement #equals and #hashCode for BitlinkModel class

--- src/main/java/elsevier/uk/entellect/platform/models/BitlinkModel.java
+++ src/main/java/elsevier/uk/entellect/platform/models/BitlinkModel.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
@@ -13,13 +15,39 @@ import org.apache.commons.lang3.builder.ToStringStyle;
 public class BitlinkModel {
 
     @JsonProperty("domain")
-    private String domain;
+    private String domain = "bit.ly";
     @JsonProperty("title")
     private String title;
     @JsonProperty("group_guid")
     private String groupGuid;
     @JsonProperty("long_url")
     private String longUrl;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BitlinkModel that = (BitlinkModel) o;
+
+        return new EqualsBuilder()
+                .append(domain, that.domain)
+                .append(title, that.title)
+                .append(groupGuid, that.groupGuid)
+                .append(longUrl, that.longUrl)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(domain)
+                .append(title)
+                .append(groupGuid)
+                .append(longUrl)
+                .toHashCode();
+    }
 
     @Override
     public String toString() {
